2a11ec80fd76be516a6e1c9ee45a3461810574f6
Xyolous <xyg.tejada@gmail.com>
2026-01-15 02:25:42 +0800

    final?

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import AuthProvider from "@/context/AuthContext";
 
 const App = () => (
     <AuthProvider>
-        <BrowserRouter>
+        <MemoryRouter>
             <AppRoutes />
-        </BrowserRouter>
+        </MemoryRouter>
     </AuthProvider>
 );
 
